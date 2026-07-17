82907b14b25aed50951e20262cbc41f6aaa2e569
K2 <ketupatel2000@gmail.com>
2026-07-17 13:16:08 -0400

    Initial Commit

---
+++ HealthIntelligence/HealthIntelligenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthIntelligenceApp.swift
+//  HealthIntelligence
+//
+//  Created by Sankalp on 2026-07-17.
+//
+
+import SwiftUI
+
+@main
+struct HealthIntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
